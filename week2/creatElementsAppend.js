85f27f6be49bee79011efec7a577b01a0e13f345
mckenzieharriman <77551017+mckenzieharriman@users.noreply.github.com>
2021-04-04 13:59:43 -0600

    Add files via upload

---
+++ week2/creatElementsAppend.js
@@ -0,0 +1,12 @@
+var h1 = document.createElement("h1")
+h1.innerText = "Welcome to my JS website"
+document.body.appendChild(h1)
+
+
+var p = document.createElement(p)
+p.innerText = "All of this was created in Javascript"
+document.body.appendChild(p)
+
+
+var li = document.querySelector(list)
+document.body.appendChild(list)
